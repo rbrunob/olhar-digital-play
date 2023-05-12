69597f6c46de5f755c7bdb4e11bddf595d88684c
rbrunob <bruno.bragaramos@gmail.com>
2023-05-12 08:12:19 -0300

    update

---
+++ src/components/news-components/news.php
@@ -0,0 +1,69 @@
+<section id=newsComponent>
+    <div class="news_component_row">
+        <div class="news_component_title">
+            <h1>Olhar Digital <span>News</span></h1>
+        </div>
+        <div class="container_news">
+            <a class="news_item">
+                <div class="news_image">
+                    <img src="./src/assets/images/noticia-panda.webp" alt="news" />
+                </div>
+                <div class="news_content">
+                    <div class="content_title">
+                        <h3>Lorem ipsum dolor</h3>
+                    </div>
+                    <div class="content_description">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, debitis
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a class="news_item">
+                <div class="news_image">
+                    <img src="./src/assets/images/noticia-panda.webp" alt="news" />
+                </div>
+                <div class="news_content">
+                    <div class="content_title">
+                        <h3>Lorem ipsum dolor</h3>
+                    </div>
+                    <div class="content_description">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, debitis
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a class="news_item">
+                <div class="news_image">
+                    <img src="./src/assets/images/noticia-panda.webp" alt="news" />
+                </div>
+                <div class="news_content">
+                    <div class="content_title">
+                        <h3>Lorem ipsum dolor</h3>
+                    </div>
+                    <div class="content_description">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, debitis
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a class="news_item">
+                <div class="news_image">
+                    <img src="./src/assets/images/noticia-panda.webp" alt="news" />
+                </div>
+                <div class="news_content">
+                    <div class="content_title">
+                        <h3>Lorem ipsum dolor</h3>
+                    </div>
+                    <div class="content_description">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, debitis
+                        </p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
